39a9ff018995373e95ac434ca5371354d023fdb9
Ken <kschaefer@harmonyhealthmedical.com>
2022-08-28 09:55:39 -0500

    adding the virtualsite script to repo

---
+++ update-virtualsite-permissions.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Virtual site folder"
+read FOLDER
+echo "Owner account"
+read ACCOUNT
+
+# Give Apache access to the files and folders in the site
+echo "Giving Apache access to $FOLDER"
+chgrp -R www-data $FOLDER
+find $FOLDER -type d -exec chmod g+rx {} +
+find $FOLDER -type d -exec chmod g+r {} +
+
+# Give owner account read/write permission to the site
+echo "Giving ownership of $FOLDER to $ACCOUNT"
+chown -R $ACCOUNT $FOLDER
+find $FOLDER -type d -exec chmod u+rxw {} +
+find $FOLDER -type f -exec chmod u+rw {} +
